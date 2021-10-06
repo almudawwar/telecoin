1b693a3e7b2553f6985c3c4332e4dd119aa5a320
Rodrigo <rodrigopg8@gmail.com>
2021-10-06 22:48:34 +0200

    Add Telegram bot listening to messages and giving basic replies

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
+require 'dotenv'
+require 'telegram/bot'
+require 'pry-byebug'
+
 class Bot
+  USERNAME = 'TelecoinCryptoBot'
+
   def initialize
-    puts 'Heyyuup'
+    puts "Heyyuup - #{Process.pid}"
+    token = Dotenv.load['TELEGRAM_TOKEN']
+
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message.text
+        when '/start'
+          bot.api.send_message(chat_id: message.chat.id, text: 'Welcome to an Altered Future...')
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: "I'm working on it, #{message.from.first_name}")
+        end
+      end
+    end
   end
 end
