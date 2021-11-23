f4d96ece4f9e373663a81ffa186749ecb7b30fe4
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2021-11-23 22:25:39 +0100

    Remove bot class

--- bin/telecoin.rb
+++ bin/telecoin.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
-require_relative '../lib/bot'
+require 'dotenv'
+require 'telegram/bot'
+require_relative '../lib/message_responder'
 
-Bot.new
+Dotenv.load
+
+# USERNAME = 'TelecoinCryptoBot'
+Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN'], logger: Logger.new($stdout)) do |bot|
+  bot.listen do |message|
+    MessageResponder.new(bot: bot, message: message).respond
+  end
+end
